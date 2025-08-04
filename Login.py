77976c727ba8e5317280ea7f1e224a34620ff529
sowmya <sowmya.at90@gmail.com>
2025-08-04 15:57:00 +0530

    orange hrm first commit

--- Login.py
+++ Login.py
@@ -6,7 +6,6 @@ driver= webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 print(driver.title)
-
-
-
-
+driver.get("http://www.rahulshettyacademy.com/")
+driver.back()
+driver.close()
